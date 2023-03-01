7629005f41c0657b2939f3fdc3b838cb346640a0
YuriyGubin <iosdevelop.2022@gmail.com>
2023-03-01 14:13:06 +0500

    Initial Commit

---
+++ ChatGPT(SUI)/ChatGPT_SUI_App.swift
@@ -0,0 +1,17 @@
+//
+//  ChatGPT_SUI_App.swift
+//  ChatGPT(SUI)
+//
+//  Created by Yuriy on 01.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct ChatGPT_SUI_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
